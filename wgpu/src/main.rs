910c2a215b5d4513abd5b56a148da157128d35dc
Mohammed Alyousef <may642_2000@hotmail.com>
2021-02-02 00:17:23 +0300

    add wgpu ex

---
+++ wgpu/src/main.rs
@@ -0,0 +1,110 @@
+use std::iter;
+use fltk::*;
+
+struct State {
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    swap_chain: wgpu::SwapChain,
+}
+
+impl State {
+    async fn new(window: &window::Window) -> Self {
+        let size = (window.width() as u32, window.height() as u32);
+
+        // The instance is a handle to our GPU
+        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                	label: Some("fltk_window"),
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                },
+                None, // Trace path
+            )
+            .await
+            .unwrap();
+
+        let sc_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.0,
+            height: size.1,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+        Self {
+            device,
+            queue,
+            swap_chain,
+        }
+    }
+
+    fn update(&mut self) {}
+
+    fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
+        let frame = self.swap_chain.get_current_frame()?.output;
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            	label: Some("render_pass"),
+                color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                    attachment: &frame.view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.5,
+                            g: 0.5,
+                            b: 0.5,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                }],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        self.queue.submit(iter::once(encoder.finish()));
+
+        Ok(())
+    }
+}
+
+fn main() {
+    let app = app::App::default();
+    let mut window = window::Window::new(100, 100, 600, 400, "WGPU window");
+    window.end();
+    window.show();
+
+    use futures::executor::block_on;
+
+    let mut state: State = block_on(State::new(&window));
+
+    window.draw(move || {
+        state.update();
+        state.render().unwrap();
+    });
+
+window.redraw();
+    while app.wait() {
+        
+    }
+}

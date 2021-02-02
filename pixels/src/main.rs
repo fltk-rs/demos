0b16ea085e289a07aa0b535b5b57601dcc74478c
Mohammed Alyousef <may642_2000@hotmail.com>
2021-02-02 15:26:54 +0300

    simplify code

--- pixels/src/main.rs
+++ pixels/src/main.rs
@@ -1,57 +1,37 @@
 use fltk::{app, prelude::*, window::Window};
-use pixels::{Error, Pixels, SurfaceTexture};
-
-pub enum Message {
-    DrawRequested,
-    Other,
-}
+use pixels::{Pixels, SurfaceTexture};
 
 const WIDTH: u32 = 320;
 const HEIGHT: u32 = 240;
 const BOX_SIZE: i16 = 64;
-
 
 struct World {
     box_x: i16,
     box_y: i16,
 }
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = app::App::default();
-    let (s, r) = app::channel::<Message>();
     let mut win = Window::default().with_size(WIDTH as i32, HEIGHT as i32).with_label("Pixels");
     win.end();
     win.show();
+
     let mut pixels = {
         let surface_texture = SurfaceTexture::new(WIDTH, HEIGHT, &win);
         Pixels::new(WIDTH, HEIGHT, surface_texture)?
     };
 
     let world = World::new();
-    win.draw(move || s.send(Message::DrawRequested));
 
-    while app.wait() {
-        if let Some(msg) = r.recv() {
-            match msg {
-                Message::DrawRequested => {
-                    world.draw(pixels.get_frame());
-                    if pixels
-                        .render()
-                        .map_err(|e| eprintln!("pixels.render() failed: {}", e))
-                        .is_err()
-                    {
-                        win.hide();
-                    }
-                }
-                _ => (),
-            }
-        }
-    }
-    Ok(())
+    win.draw(move || {
+        world.draw(pixels.get_frame());
+        pixels.render().unwrap();
+    });
+
+    Ok(app.run()?)
 }
 
 impl World {
-    /// Create a new `World` instance that can draw a moving box.
     fn new() -> Self {
         Self {
             box_x: 24,
@@ -59,9 +39,6 @@ impl World {
         }
     }
 
-    /// Draw the `World` state to the frame buffer.
-    ///
-    /// Assumes the default texture format: `wgpu::TextureFormat::Rgba8UnormSrgb`
     fn draw(&self, frame: &mut [u8]) {
         for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
             let x = (i % WIDTH as usize) as i16;

793f7dc15ceec8c30255729f688bc29734c2d950
Mohammed Alyousef <may642_2000@hotmail.com>
2021-03-01 20:51:52 +0300

    modify speedy2d example

--- speedy2d/src/main.rs
+++ speedy2d/src/main.rs
@@ -1,7 +1,8 @@
 use fltk::{
     app,
-    prelude::{WidgetExt, GroupExt, WindowExt},
-    window::GlutWindow,
+    enums::Event,
+    prelude::{WidgetBase, WidgetExt, GroupExt, WindowExt},
+    window::{GlutWindow, Window},
     utils
 };
 use speedy2d::GLRenderer;
@@ -10,36 +11,45 @@ use speedy2d::dimen::Vector2;
 use speedy2d::image::{ImageDataType, ImageSmoothingMode};
 
 fn main() {
-    let mut fb: Vec<u8> = vec![0u8; 128 * 128 * 3];
+    let mut fb: Vec<u8> = vec![0u8; 300 * 300 * 3];
     for (iter, pixel) in fb.chunks_exact_mut(3).enumerate() {
-        let x = iter % 128;
-        let y = iter / 128;
+        let x = iter % 300;
+        let y = iter / 300;
         let (red, green, blue) = utils::hex2rgb((x ^ y) as u32);
         pixel.copy_from_slice(&[red, green, blue]);
     }
 
     let app = app::App::default();
-    let mut win = GlutWindow::default().with_size(640, 480);
+    let mut main_win = Window::default().with_size(800, 600);
+    let mut win = GlutWindow::default().with_size(300, 300).center_of(&main_win);
     win.end();
-    win.show();
+    main_win.end();
+    main_win.show();
     win.make_current();
+
+    win.handle(|ev| match ev {
+        Event::Push => {
+            println!("Pushed");
+            true
+        },
+        _ => false,
+    });
 
     gl::load_with(|s| win.get_proc_address(s));
 
-    let mut renderer = unsafe { GLRenderer::new_for_current_context((640, 480)) }.unwrap();
+    let mut renderer = unsafe { GLRenderer::new_for_current_context((300, 300)) }.unwrap();
 
     renderer.draw_frame(|graphics| {
         graphics.clear_screen(Color::WHITE);
-        graphics.draw_circle((100.0, 100.0), 75.0, Color::BLUE);
         let handle = graphics
         .create_image_from_raw_pixels(
             ImageDataType::RGB,
             ImageSmoothingMode::Linear,
-            Vector2::new(128, 128),
+            Vector2::new(300, 300),
             &fb,
         )
         .unwrap();
-        graphics.draw_image(Vector2::new(200., 200.), &handle);
+        graphics.draw_image(Vector2::new(0., 0.), &handle);
     });
 
     app.run().unwrap();

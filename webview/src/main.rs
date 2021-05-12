7c9ea988f2ca6428728193a849785731cc0b6b7f
MoAlyousef <may642_2000@hotmail.com>
2021-05-12 18:54:29 +0300

    try adding linux support

--- webview/src/main.rs
+++ webview/src/main.rs
@@ -1,6 +1,9 @@
 use fltk::{enums::Color, prelude::*, *};
 use webview_official_sys as wv;
 
+#[cfg(target_os = "linux")]
+use gdk_x11_sys as gdk;
+
 fn main() {
     let app = app::App::default().with_scheme(app::AppScheme::Gtk);
     let mut win = window::Window::default()
@@ -16,12 +19,27 @@ fn main() {
     win.show();
 
     let w;
+    #[cfg(not(target_os = "linux"))]
     unsafe {
         use std::os::raw;
         w = wv::webview_create(
             0,
             &mut inner_win.raw_handle() as *mut *mut raw::c_void as *mut raw::c_void,
         );
+    }
+    #[cfg(target_os = "linux")]
+    unsafe {
+        gtk_sys::gtk_init(&mut 0, std::ptr::null_mut());
+        let mn = gdk_sys::gdk_display_manager_get();
+        // fltk::app::display() doesn't work for some reason
+        let display = gdk_sys::gdk_display_manager_open_display(mn, concat!(env!("DISPLAY"), "\0").as_ptr() as _);
+        let gdkwin = gdk::gdk_x11_window_foreign_new_for_display(
+            display as _,
+            inner_win.raw_handle(),
+        );
+        let gtkwid = gtk_sys::gtk_window_new(0);
+        gtk_sys::gtk_widget_set_window(gtkwid, gdkwin);
+        w = wv::webview_create(0, gtkwid as _);
     }
     assert!(!w.is_null());
 
@@ -30,6 +48,7 @@ fn main() {
     }
 
     app.run().unwrap();
+
     unsafe {
         wv::webview_destroy(w);
     }

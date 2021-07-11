b9a2b5b660846573fa041bfff7ac1461c96414a3
Mohammed Alyousef <may642_2000@hotmail.com>
2021-07-11 15:50:01 +0300

    Update main.rs

--- webview/src/main.rs
+++ webview/src/main.rs
@@ -1,7 +1,7 @@
-use fltk::{prelude::*, *};
+use fltk::{app, enums::Event, prelude::*, window};
 
 fn main() {
-    let app = app::App::default();
+    let _app = app::App::default();
     let mut win = window::Window::default()
         .with_size(800, 600)
         .with_label("Webview");
@@ -10,9 +10,17 @@ fn main() {
         .center_of_parent();
     win.end();
     win.show();
+
+    // close the app when the main window is closed
+    win.set_callback(|_| {
+        if app::event() == Event::Close {
+            std::process::exit(0);
+        }
+    });
+
+    let mut wv = fltk_webview::Webview::create(false, &mut wv_win);
+    wv.navigate("https://google.com");
     
-    let mut wv = fltk_webview::Webview::from(false, &mut wv_win);
-    wv.navigate("http://wikipedia.com");
-    
+    // the webview handles the main loop
     wv.run();
 }

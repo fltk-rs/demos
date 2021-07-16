92109894b2cc61db609128bdcec8b4ffd2f201bf
Mohammed Alyousef <may642_2000@hotmail.com>
2021-07-16 17:19:05 +0300

    Update main.rs

--- webview/src/main.rs
+++ webview/src/main.rs
@@ -11,13 +11,6 @@ fn main() {
     win.end();
     win.show();
 
-    // close the app when the main window is closed
-    win.set_callback(|_| {
-        if app::event() == Event::Close {
-            std::process::exit(0);
-        }
-    });
-
     let mut wv = fltk_webview::Webview::create(false, &mut wv_win);
     wv.navigate("https://google.com");
     

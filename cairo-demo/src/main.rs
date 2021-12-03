21b25a3464b2e13962a3fa7e87bb80aa3dc1f12e
Mohammed Alyousef <may642_2000@hotmail.com>
2021-12-03 21:23:08 +0300

    Update main.rs

--- cairo-demo/src/main.rs
+++ cairo-demo/src/main.rs
@@ -14,12 +14,12 @@ fn convert_to_rgba(arr: &[u8]) -> Vec<u8> {
 }
 
 #[derive(Clone)]
-struct MyWidget {
+struct CairoWidget {
     frm: frame::Frame,
     ctx: Context,
 }
 
-impl MyWidget {
+impl CairoWidget {
     pub fn new(x: i32, y: i32, w: i32, h: i32, label: &str) -> Self {
         let frm = frame::Frame::new(x, y, w, h, None).with_label(label);
         let surface = ImageSurface::create(Format::ARgb32, w, h).expect("Couldn’t create surface");
@@ -35,13 +35,13 @@ impl MyWidget {
     }
 }
 
-fltk::widget_extends!(MyWidget, frame::Frame, frm);
+fltk::widget_extends!(CairoWidget, frame::Frame, frm);
 
 fn main() {
     let app = app::App::default().with_scheme(app::AppScheme::Gtk);
     let mut win = window::Window::new(100, 100, 600, 600, "Cairo");
 
-    let mut wid = MyWidget::new(0, 0, 600, 600, "Label");
+    let mut wid = CairoWidget::new(0, 0, 600, 600, "Label");
 
     wid.draw(move |w| {
         let ctx = &w.ctx;

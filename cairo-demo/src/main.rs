b51857f36bb7f920bef34ec917d5c3e6defb8cbc
MoAlyousef <may642_2000@hotmail.com>
2023-07-29 23:34:02 +0300

    update cairo demo

--- cairo-demo/src/main.rs
+++ cairo-demo/src/main.rs
@@ -1,44 +1,58 @@
-use cairo::{
-    Context,
-    Format,
-    ImageSurface
-};
-use fltk::{
-    enums::*,
-    prelude::*,
-    *,
-    image::IcoImage
-};
-use std::convert::TryFrom;
+use cairo::{Context, Format, ImageSurface};
+use fltk::{enums::*, prelude::*, draw::Rect, *};
+use std::{cell::RefCell, rc::Rc};
 
-fn convert_to_rgba(arr: &[u8]) -> Vec<u8> {
-    let mut v = vec![];
-    for (_, pixel) in arr.chunks_exact(4).enumerate() {
-        v.push(pixel[2]);
-        v.push(pixel[1]);
-        v.push(pixel[0]);
-        v.push(pixel[3]);
-    }
-    v
+fn draw_box_with_alpha(ctx: Context, rect: &Rect, color: Color, alpha: u8) {
+    let (r, g, b) = color.to_rgb();
+    ctx.save().unwrap();
+    ctx.move_to(rect.x as f64, rect.y as f64);
+    ctx.line_to((rect.x + rect.w) as f64, rect.y as f64);
+    ctx.line_to((rect.x + rect.w) as f64, (rect.y + rect.h) as f64);
+    ctx.line_to(rect.x as f64, (rect.y + rect.h) as f64);
+    ctx.close_path();
+    ctx.set_source_rgba(r as f64 / 255.0, g as f64 / 255.0, b as f64 / 255.0, alpha as f64 / 255.0);
+    ctx.fill().unwrap();
+    ctx.restore().unwrap();
 }
 
 #[derive(Clone)]
 struct CairoWidget {
     frm: frame::Frame,
-    ctx: Context,
+    alpha: Rc<RefCell<u8>>,
 }
 
 impl CairoWidget {
     pub fn new(x: i32, y: i32, w: i32, h: i32, label: &str) -> Self {
-        let frm = frame::Frame::new(x, y, w, h, None).with_label(label);
-        let surface = ImageSurface::create(Format::ARgb32, w, h).expect("Couldn’t create surface");
-        let ctx = Context::new(&surface).unwrap();
-        Self { frm, ctx }
+        let mut frm = frame::Frame::new(x, y, w, h, None).with_label(label);
+        frm.super_draw_first(false); // required for windows
+        let alpha = Rc::from(RefCell::from(255));
+        frm.draw({
+            let alpha = alpha.clone();
+            move |w| {
+                let cc = app::cairo::cc();
+                let ctx = unsafe { Context::from_raw_none(cc as _) };
+                draw_box_with_alpha(ctx, &Rect {x : w.x(), y : w.y(), w : w.w(), h : w.h()}, w.color(), *alpha.borrow());
+                unsafe {
+                    app::cairo::flush(cc); // required for windows
+                }
+            }
+        });
+        Self { frm, alpha }
+    }
+
+    pub fn set_alpha(&mut self, val: u8) {
+        *self.alpha.borrow_mut() = val;
+    }
+
+    pub fn alpha(&self) -> u8 {
+        *self.alpha.borrow()
     }
 
     pub fn draw<F: FnMut(&mut Self) + 'static>(&mut self, mut cb: F) {
         let mut frm = self.clone();
+        let mut old_cb = unsafe { frm.draw_data().unwrap() };
         self.frm.draw(move |_| {
+            old_cb();
             cb(&mut frm);
         });
     }
@@ -48,25 +62,21 @@ fltk::widget_extends!(CairoWidget, frame::Frame, frm);
 
 fn main() {
     let app = app::App::default().with_scheme(app::AppScheme::Gtk);
+    app::cairo::set_autolink_context(true);
     let mut win = window::Window::new(100, 100, 600, 600, "Cairo");
     let icon: IcoImage = IcoImage::load(&std::path::Path::new("src/fltk.ico")).unwrap();
     win.make_resizable(true);
     win.set_icon(Some(icon));
 
-    let mut wid = CairoWidget::new(0, 0, 600, 600, "Label");
-
-    wid.draw(move |w| {
-        let ctx = &w.ctx;
-        ctx.set_source_rgb(0.0, 0.0, 1.0);
-        ctx.paint().unwrap();
-        let surface = ImageSurface::try_from(ctx.target()).unwrap();
-        surface
-            .with_data(|s| {
-                let temp = convert_to_rgba(s);
-                draw::draw_image(&temp, w.x(), w.y(), 600, 600, ColorDepth::Rgba8).unwrap();
-            })
-            .unwrap();
-    });
+    let mut box1 = CairoWidget::new(0, 0, 100, 100, "Box1");
+    box1.set_color(Color::from_rgb(0, 0, 255));
+    box1.set_alpha(100);
+    let mut box2 = CairoWidget::new(75, 75, 100, 100, "Box2");
+    box2.set_color(Color::Red);
+    box2.set_alpha(100);
+    let mut box3 = CairoWidget::new(150, 150, 100, 100, "Box3");
+    box3.set_color(Color::Green);
+    box3.set_alpha(100);
 
     win.end();
     win.show();
